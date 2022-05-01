cab1b63c67db68a80d6feb57599e475533e6be4f
yildizemre2021 <yildiz.emre.2021@gmail.com>
2022-05-01 22:15:39 +0300

    Versiyon 1.0

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿int tahmax = 0;
+int x = 1, y = 100;
+
+Random rasgele = new Random();
+
+Console.Clear();
+
+Console.WriteLine("1-100 arasi bir sayi tutunuz");
+int sayi = int.Parse(Console.ReadLine());
+while (true)
+{
+    int tahmin = rasgele.Next(x, y);
+
+    if (tahmin < sayi)
+    {
+        tahmax++;
+        x = tahmin;
+
+        Console.WriteLine("AYI tahminini " + tahmin + "  olarak yapti.");
+        Console.WriteLine("Yeniden tahmin yapiyor...");
+        Console.ReadKey();
+    }
+    else if (tahmin > sayi)
+    {
+        tahmax++;
+        y = tahmin + 1;
+        Console.WriteLine("AYI tahminini " + tahmin + "  olarak yapti.");
+        Console.WriteLine("Yeniden tahmin yapiyor...");
+        Console.ReadKey();
+    }
+    else if (tahmin == sayi)
+    {
+        tahmax++;
+        Console.WriteLine("Tebrikler " + sayi + " sayisini " + tahmax + ". tahmininizde " + "buldunuz!");
+        break;
+    }
+}
+
+
+
+
